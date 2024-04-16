7559a603d3ed8021cceb844419991b97381be889
RomanSushchanka <vadnatrom@mail.ru>
2024-04-16 17:22:07 +0300

    Home Work 12

---
+++ HomeWork12/HW02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Gender
+{
+    Female,
+    Male
+}
+
+public class Person : IComparable<Person>
+{
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
+
+    public int CompareTo(Person other)
+    {
+        if (this.Gender != other.Gender)
+        {
+            return this.Gender.CompareTo(other.Gender);
+        }
+        else
+        {
+            return this.Age.CompareTo(other.Age);
+        }
+    }
+}
+
+public class PersonComparer : IComparer<Person>
+{
+    public int Compare(Person x, Person y)
+    {
+        if (x.Gender != y.Gender)
+        {
+            return x.Gender.CompareTo(y.Gender);
+        }
+        else
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> listOfPerson = new List<Person>
+        {
+            new Person { Age = 25, Gender = Gender.Female },
+            new Person { Age = 30, Gender = Gender.Male },
+            new Person { Age = 20, Gender = Gender.Female },
+            new Person { Age = 35, Gender = Gender.Male },
+            new Person { Age = 22, Gender = Gender.Female },
+            new Person { Age = 28, Gender = Gender.Male },
+            new Person { Age = 27, Gender = Gender.Female },
+            new Person { Age = 32, Gender = Gender.Male },
+            new Person { Age = 23, Gender = Gender.Female },
+            new Person { Age = 31, Gender = Gender.Male }
+        };
+
+        listOfPerson.Sort();
+
+        Console.WriteLine("Сортировка с использованием IComparable<Person>:");
+        foreach (var person in listOfPerson)
+        {
+            Console.WriteLine($"Возраст: {person.Age}, Пол: {person.Gender}");
+        }
+
+        Person[] array = listOfPerson.ToArray();
+        Array.Sort(array, new PersonComparer());
+
+        Console.WriteLine("\nСортировка с использованием PersonComparer:");
+        foreach (var person in array)
+        {
+            Console.WriteLine($"Возраст: {person.Age}, Пол: {person.Gender}");
+        }
+    }
+}
---
+++ HomeWork12/HW03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MobileNetwork
+{
+    public string Name { get; set; }
+}
+
+public class MobilePhone<T> where T : MobileNetwork
+{
+    public T Network { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MobileNetwork network = new MobileNetwork { Name = "MyNetwork" };
+        MobilePhone<MobileNetwork> phone1 = new MobilePhone<MobileNetwork> { Network = network };
+
+        Console.WriteLine($"Phone1 Network: {phone1.Network.Name}");
+
+        MobileNetwork childNetwork = new MobileNetwork { Name = "ChildNetwork" };
+        MobilePhone<MobileNetwork> phone2 = new MobilePhone<MobileNetwork> { Network = childNetwork };
+
+        Console.WriteLine($"Phone2 Network: {phone2.Network.Name}");
+    }
+}
