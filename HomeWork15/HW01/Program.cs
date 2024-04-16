84dff93954a4e2125f48625f0105b8c1ceb455a5
RomanSushchanka <vadnatrom@mail.ru>
2024-04-16 18:05:25 +0300

    Home Work 15

---
+++ HomeWork15/HW01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class School
+{
+    public delegate void StudentAddedDelegate(string name, int age);
+    public event StudentAddedDelegate StudentAdded;
+
+    public void AddStudent(string name, int age)
+    {
+        Console.WriteLine($"Ученик {name} добавлен в школу.");
+        StudentAdded?.Invoke(name, age);
+    }
+}
+
+class Director
+{
+    public void GreetParents(string name, int age)
+    {
+        Console.WriteLine($"Добро пожаловать в школу, родители {name}! Ваш ребенок {age} лет.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        School school = new School();
+        Director director = new Director();
+        school.StudentAdded += director.GreetParents;
+        school.AddStudent("Валера", 12);
+        school.AddStudent("Коля", 11);
+    }
+}
---
+++ HomeWork15/HW02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Kindergarten
+{
+    public event EventHandler ChildGraduated;
+    public void GraduateChild(string childName)
+    {
+        Console.WriteLine($"Ребенок {childName} окончил детский сад.");
+        ChildGraduated?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class School
+{
+    public void AcceptChild(object sender, EventArgs e)
+    {
+        Console.WriteLine("Ребенок принят в школу.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kindergarten kindergarten = new Kindergarten();
+        School school = new School();
+        kindergarten.ChildGraduated += school.AcceptChild;
+        kindergarten.GraduateChild("Гоша");
+        Console.ReadKey();
+    }
+}
