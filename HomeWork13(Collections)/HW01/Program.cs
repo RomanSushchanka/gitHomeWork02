8d4326801fbd9bee1a4100e567c956f750212747
RomanSushchanka <vadnatrom@mail.ru>
2024-04-16 17:29:27 +0300

    Home Work 13 (Collections)

---
+++ HomeWork13(Collections)/HW01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static IEnumerable<string> GetWordsGreaterThanFive(string[] words)
+    {
+        foreach (var word in words)
+        {
+            if (word.Length > 5)
+            {
+                yield return word;
+            }
+        }
+    }
+
+    static string[] GenerateRandomWords(int count)
+    {
+        Dictionary<int, string> wordDictionary = new Dictionary<int, string>()
+        {
+            { 1, "apple" },
+            { 2, "banana" },
+            { 3, "cherry" },
+            { 4, "date" },
+            { 5, "elderberry" },
+            { 6, "fig" },
+            { 7, "grape" },
+            { 8, "honeydew" },
+            { 9, "kiwi" },
+            { 10, "lemon" }
+        };
+
+        Random random = new Random();
+        List<string> randomWords = new List<string>();
+
+        while (randomWords.Count < count)
+        {
+            int randomKey = random.Next(1, wordDictionary.Count + 1);
+            string randomWord = wordDictionary[randomKey];
+
+            if (!randomWords.Contains(randomWord))
+            {
+                randomWords.Add(randomWord);
+            }
+        }
+
+        return randomWords.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        string[] randomWords = GenerateRandomWords(3);
+
+        Console.WriteLine("Слова длиной более 5 символов:");
+
+        foreach (var word in GetWordsGreaterThanFive(randomWords))
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ HomeWork13(Collections)/HW02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Collection<T> where T : IComparable<T>
+{
+    private List<T> items = new List<T>();
+
+    public void Add(T item)
+    {
+        items.Add(item);
+        items.Sort();
+    }
+
+    public void Display()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class MyClass : IComparable<MyClass>
+{
+    public int IdNumber { get; set; }
+
+    public int CompareTo(MyClass other)
+    {
+        return IdNumber.CompareTo(other.IdNumber);
+    }
+
+    public override string ToString()
+    {
+        return $"IdNumber: {IdNumber}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Collection<MyClass> collection = new Collection<MyClass>();
+
+        collection.Add(new MyClass { IdNumber = 3 });
+        collection.Add(new MyClass { IdNumber = 1 });
+        collection.Add(new MyClass { IdNumber = 2 });
+
+        collection.Display();
+
+        collection.Add(new MyClass { IdNumber = 4 });
+
+        collection.Display();
+    }
+}
